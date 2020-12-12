2277e622ee3946c3aa24343c9259932c8d66af6f
splucena <sumilangplucena@yahoo.com.ph>
2020-12-13 01:19:11 +0800

    added ajax order detail

--- routes/orders.js
+++ routes/orders.js
@@ -15,7 +15,23 @@ router.get("/order_detail/:id", async (req, res, next) => {
 	let orderid = req.params.id;
 	let order = await orders.getOrderDetail(orderid);
 	//console.log(order);
-	res.send({ order: order });
+	res.send({ order: order, s_username: req.session.username });
+});
+
+// Create Order
+router.get("/create_order", (req, res, next) => {
+	res.render("pages/orderedit", {
+		title: "Create Order",
+		docreate: true,
+		orderid: undefined,
+		order_no: undefined,
+		date_created: undefined,
+		shipping_date: undefined,
+		total_amount: undefined,
+		processed_by: undefined,
+		order: false,
+		s_username: req.session.username,
+	});
 });
 
 module.exports = router;
